686c7e4d6cc8462568b3100dd5df315e53661095
Mouhsssine Nabil <46906074+cnabilhub@users.noreply.github.com>
2023-09-14 11:34:44 +0100

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,11 @@
 def square_matrix_simple(matrix=[]):
-    new_matrix =[]
+    if not matrix:
+        return []
+    new_matrix = []
     for row in matrix:
         sub_matrix = []
         for item in row:
             sub_matrix.append(item*item)
 
         new_matrix.append(sub_matrix)
-    return  new_matrix
+    return new_matrix
