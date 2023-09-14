a2386f8f753fa96930ab52fe212a3bce76927a8a
Mouhsssine Nabil <46906074+cnabilhub@users.noreply.github.com>
2023-09-14 11:46:24 +0100

    fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,14 @@
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        return []
-    new_matrix = []
-    for row in matrix:
-        sub_matrix = []
-        for item in row:
-            sub_matrix.append(item*item)
+    # Get the number of rows and columns in the input matrix
+    num_rows = len(matrix)
+    num_cols = len(matrix[0]) if num_rows > 0 else 0
 
-        new_matrix.append(sub_matrix)
-    return new_matrix
+    # Create a new matrix to store the squared values
+    result_matrix = [[0] * num_cols for _ in range(num_rows)]
+
+    # Loop through the elements of the input matrix and compute their squares
+    for i in range(num_rows):
+        for j in range(num_cols):
+            result_matrix[i][j] = matrix[i][j] ** 2
+
+    return result_matrix
